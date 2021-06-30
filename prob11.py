c993a2dcb55a7a0b4b7416b112eca444451d04e8
Anshuman Thakur <77040583+anshuman2197@users.noreply.github.com>
2021-06-30 18:21:02 +0530

    Add files via upload

---
+++ prob11.py
@@ -0,0 +1,26 @@
+w="welcome to Web Tech"
+# n=w.lower()
+# print(n)
+
+# n=w.upper()
+# print(n)
+
+# n=w.title()
+# print(n)
+
+# n=w.capitalize()
+# print(n)
+
+# print(w.find('T'))
+# print(w.find('e',7))
+
+# print(w.index('c',5))
+
+# print(w.find('z'))
+
+# print(w.isalpha())
+
+# print(w.isdigit())
+
+# print(w.isalnum())
+
